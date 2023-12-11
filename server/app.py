92f6ae230b58cabfd0fe5d9b0c53b4928a1b8a8a
Karan Bhullar <cswailord@gmail.com>
2023-12-11 03:58:18 -0500

    Functional Database

--- server/app.py
+++ server/app.py
@@ -1,12 +1,13 @@
 import os
-import time
+
+from database import db
 from flask import Flask, jsonify, render_template, request
 from flask_cors import CORS
 from flask_socketio import SocketIO, emit
 
-
 #global variables
 currUsers = []
+database = db.db()
 
 # instantiate the app, points to the production distribution
 app = Flask(__name__, template_folder=os.path.abspath('../client/dist'), static_folder=os.path.abspath('../client/dist/assets'), static_url_path='/assets')
@@ -31,7 +32,7 @@ def userJoin():
 #Socket.io
 @socketio.on('send_message')
 def send_message(data):
-    
+    database.insert(data)
     print(data)
     emit('send_message', data)
 
--- server/database/db.py
+++ server/database/db.py
@@ -1,18 +1,38 @@
 import psycopg2
+from psycopg2.extras import Json
+from psycopg2.extensions import register_adapter
 import os
 
-try:
-    #Make sure all the information matches with your postgres server
-    connection = psycopg2.connect(
-        user='postgres',
-        password=os.environ.get('POSTGRES_PASSWORD'),
-        port= '5433'
-    )
-    print("Successfully connected to the database.")
-except:
-    print("Could not successfully connect to the database")
+class db:
+    def __init__(self):
+        register_adapter(dict, Json)
 
-# cursor = connection.cursor()
-# create = "CREATE DATABASE logs;"
-# cursor.execute(create)
+        DATABASE_CONFIG = { 
+            "database": "logs", 
+            "user": "postgres", 
+            "password": os.environ.get('POSTGRES_PASSWORD'), 
+            "host": "localhost", 
+            "port":  5433, 
+        } 
 
+        #Make sure all the information matches with your postgres server
+        self.connection = psycopg2.connect(
+            database= DATABASE_CONFIG["database"],
+            user=DATABASE_CONFIG["user"],
+            password=DATABASE_CONFIG["password"],
+            host=DATABASE_CONFIG["host"],
+            port= DATABASE_CONFIG["port"]
+        )
+        self.connection.autocommit = True
+        self.cursor = self.connection.cursor()
+
+        table_info = '''
+        CREATE TABLE IF NOT EXISTS chat_data (
+            id SERIAL PRIMARY KEY,
+            data VARCHAR(500) NOT NULL
+        )
+        '''
+        self.cursor.execute(table_info)
+
+    def insert(self, message_data):
+        self.cursor.execute("INSERT INTO chat_data (data) VALUES (%s)", [message_data])
